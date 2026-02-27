b1221fddc017dec97ba4d067938660371c1d106d
AhmedSalafi <ryankhann47@gmail.com>
2026-02-26 21:06:12 -0500

    implemented renderItems() and removeItem() functions

--- persistent-todo/index.js
+++ persistent-todo/index.js
@@ -1,10 +1,31 @@
-let items = [];
+let items = []; // this is like a db for persistence, except that its client side
 
-function loadItems() {
-
-}
+const itemsDiv = document.getElementById("items");
 
 function renderItems() {
+    itemsDiv.innerHTML = null; // fresh slate to start adding items
+
+    for (const [idx, item] of Object.entries(items)) {
+        const container = document.createElement("div");
+        container.style.marginBottom = "10px";
+
+        const text = document.createElement("p");
+        text.style.display = "inline";
+        text.style.marginRight = "10px";
+        text.textContent = item;
+
+        const button = document.createElement("button");
+        button.textContent = "Delete";
+        button.onclick = () => removeItem(idx) // js calls the function immediately if not wrapped as such 
+
+        container.appendChild(text);
+        container.appendChild(button);
+
+        itemsDiv.appendChild(container);
+    }
+}
+
+function loadItems() {
 
 }
 
@@ -16,6 +37,7 @@ function addItem() {
 
 }
 
-function removeItem() {
-
+function removeItem(idx) {
+    items.splice(idx, 1);
+    renderItems();
 }
