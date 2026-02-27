8a9dfdc36b4bf8e7c3a44d8344bb0bcc2d415a35
AhmedSalafi <ryankhann47@gmail.com>
2026-02-27 02:00:00 -0500

    deleted everything from file, start from scratch

--- nxn-tic-tac-toe/nxn-tic-tac-toe.js
+++ nxn-tic-tac-toe/nxn-tic-tac-toe.js
@@ -1,23 +1,2 @@
-/*
-We want to build a tic-tac-toe game that looks like this:
 
-0 1 2
-x o x 0
-o x x 1
-x o o 2
 
-So we can represent the board as a multi-dim array.
-
-Let's represent empty spaces with "."s for convenience.
-
-I think it should be turn based between player 1 &
-player 2.
-
-At a given player's turn, they are prompted twice: 
-once for the horizontal position, once for vertical.
-
-At the end of each player's turn, the whole board is
-printed.
-
-To determine if a game has been won: 
-*/
